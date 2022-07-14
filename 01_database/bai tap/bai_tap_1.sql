227b3ae2e6040496d26ece7defa9036918113168
lehongson1998 <lhongson2909@gmail.com>
2022-07-14 08:55:12 +0700

    fix comment

--- 01_database/bai tap/bai_tap_1.sql
+++ 01_database/bai tap/bai_tap_1.sql
@@ -1,16 +1,26 @@
-create database student_management;
-use student_management;
-create table class (
-id int primary key auto_increment,
-`name` varchar(45)
+DROP DATABASE IF EXISTS student_management;
+CREATE DATABASE student_management;
+USE student_management;
+
+CREATE TABLE class (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(45)
 );
-insert into class (`name`) values ('le hong son'),('nguyen tan tai');
-select * from class;
-create table teacher (
-id int primary key auto_increment,
-`name` varchar(45),
-age int,
-country varchar(45)
+
+INSERT INTO class (`name`) VALUES ('le hong son'),('nguyen tan tai');
+
+SELECT 
+    *
+FROM
+    class;
+CREATE TABLE teacher (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(45),
+    age INT,
+    country VARCHAR(45)
 );
-insert into teacher (`name`,age,country) values ('le hong son', 24 , 'viet nam'),('nguyen tan tai', 26, 'trung quoc');
-select * from teacher;
+INSERT INTO teacher (`name`,age,country) VALUES ('le hong son', 24 , 'viet nam'),('nguyen tan tai', 26, 'trung quoc');
+SELECT 
+    *
+FROM
+    teacher;
---
+++ ss2_design_database/thuc_hanh/thuc_hanh_2.sql
@@ -0,0 +1,37 @@
+CREATE DATABASE quan_ly_diem_thi;
+USE quan_ly_diem_thi;
+CREATE TABLE hoc_sinh(
+	MaHS VARCHAR(20) PRIMARY KEY,
+    TenHS VARCHAR(50),
+    NgaySinh DATE,
+    Lop VARCHAR(20),
+    GT VARCHAR(20)
+);
+
+CREATE TABLE mon_hoc (
+    MaMH VARCHAR(20) PRIMARY KEY,
+    TenMH VARCHAR(50)
+);
+
+CREATE TABLE BangDiem(
+    MaHS VARCHAR(20),
+    MaMH VARCHAR(20),
+    DiemThi INT,
+    NgayKT DATETIME,
+    PRIMARY KEY (MaHS, MaMH),
+    FOREIGN KEY (MaHS) REFERENCES hoc_sinh(MaHS),
+    FOREIGN KEY (MaMH) REFERENCES mon_hoc(MaMH)
+);
+
+CREATE TABLE GiaoVien(
+    MaGV VARCHAR(20) PRIMARY KEY,
+    TenGV VARCHAR(20),
+    SDT VARCHAR(10)
+);
+
+ALTER TABLE mon_hoc ADD MaGV VARCHAR(20);
+ALTER TABLE mon_hoc ADD CONSTRAINT FK_MaGV FOREIGN KEY (MaGV) REFERENCES GiaoVien(MaGV);
+
+select * from mon_hoc;
+
+
